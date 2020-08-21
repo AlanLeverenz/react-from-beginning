1829e956ef950b73b859fe25b73ae07fe3ac8ca1
Alan Leverenz <awleverenz@aol.com>
2020-08-20 20:59:54 -0400

    lecture16 array map of component

---
+++ SECTION1/lecture16-arrayOfComponents/data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    course: 'React From the Beginning',
+    instructor: 'Robert Bunch',
+    image: 'https://udemy-images.udemy.com/course/100x100/2195280_49b2.jpg',
+    id: 1,
+  },
+  {
+    course: 'Apache Kafka Series',
+    instructor: 'Stephane Maarek',
+    image: 'https://udemy-images.udemy.com/course/480x270/1141696_6151_3.jpg',
+    id: 2,
+  },
+  {
+    course: 'Music Production in Logic Pro X',
+    instructor: 'Tomas George',
+    image: 'https://udemy-images.udemy.com/course/480x270/897192_2cee_6.jpg',
+    id: 3,
+  },
+  {
+    course: 'Unity Game Development',
+    instructor: 'Jonathan Weinberger',
+    image: 'https://udemy-images.udemy.com/course/480x270/1328572_b05d_4.jpg',
+    id: 4,
+  },
+];
