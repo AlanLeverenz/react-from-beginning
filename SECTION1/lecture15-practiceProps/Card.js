bd43b311607c4e4f431cee2fd71a748206c04bdb
Alan Leverenz <awleverenz@aol.com>
2020-08-20 20:22:33 -0400

    updated lecture15 myway or the bunch way

--- SECTION1/lecture15-practiceProps/Card.js
+++ SECTION1/lecture15-practiceProps/Card.js
@@ -1,14 +1,15 @@
+// Card component
 function Card(props) {
   console.log(props);
   return (
     <div className='col s3 m3'>
       <div className='card hoverable medium'>
         <div className='card-image'>
-          <img src={props.img} />
+          <img src={props.data.image} />
         </div>
         <div className='card-content'>
-          <p>{props.title}</p>
-          <p>{props.name}</p>
+          <p>{props.data.course}</p>
+          <p>{props.data.instructor}</p>
         </div>
         <div className='card-action'>
           <a href='#'>$9.99</a>
@@ -17,3 +18,24 @@ function Card(props) {
     </div>
   );
 }
+
+// MY WAY
+// function Card(props) {
+//   console.log(props);
+//   return (
+//     <div className='col s3 m3'>
+//       <div className='card hoverable medium'>
+//         <div className='card-image'>
+//           <img src={props.img} />
+//         </div>
+//         <div className='card-content'>
+//           <p>{props.title}</p>
+//           <p>{props.name}</p>
+//         </div>
+//         <div className='card-action'>
+//           <a href='#'>$9.99</a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
--- SECTION1/lecture15-practiceProps/app.js
+++ SECTION1/lecture15-practiceProps/app.js
@@ -1,33 +1,43 @@
 // ReactDOM.render here
-// ReactDOM.render(
-//     <h1>Sanity Check</h1>,
-//     document.getElementById('root')
-// )
+ReactDOM.render(
+  <div className='row'>
+    <Card data={data[0]} />
+    <Card data={data[1]} />
+    <Card data={data[2]} />
+    <Card data={data[3]} />
+
+    {/* <Card name={data[0].instructor} title={data[0].course} image={data[0].image} />
+        <Card name={data[1].instructor} title={data[1].course} image={data[1].image} />
+        <Card name={data[2].instructor} title={data[2].course} image={data[2].image} />
+        <Card name={data[3].instructor} title={data[3].course} image={data[3].image} /> */}
+  </div>,
+  document.getElementById('root')
+);
 
 // MY WAY
-const App = () => {
-  return (
-    <div className='row'>
-      <Card
-        name={data[0].instructor}
-        title={data[0].course}
-        img={data[0].image}
-      />
-      <Card
-        name={data[1].instructor}
-        title={data[1].course}
-        img={data[1].image}
-      />
-      <Card
-        name={data[2].instructor}
-        title={data[2].course}
-        img={data[2].image}
-      />
-      <Card
-        name={data[3].instructor}
-        title={data[3].course}
-        img={data[3].image}
-      />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div className='row'>
+//       <Card
+//         name={data[0].instructor}
+//         title={data[0].course}
+//         img={data[0].image}
+//       />
+//       <Card
+//         name={data[1].instructor}
+//         title={data[1].course}
+//         img={data[1].image}
+//       />
+//       <Card
+//         name={data[2].instructor}
+//         title={data[2].course}
+//         img={data[2].image}
+//       />
+//       <Card
+//         name={data[3].instructor}
+//         title={data[3].course}
+//         img={data[3].image}
+//       />
+//     </div>
+//   );
+// };
