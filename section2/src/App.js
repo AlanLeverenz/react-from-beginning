7baaa46c76ed18830c204375ff004f588ccccddd
Alan Leverenz <awleverenz@aol.com>
2020-08-25 15:21:15 -0400

    Update state with events

--- section2/src/App.js
+++ section2/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 // import StateInAction from './StateInAction';
-import SimpleEvents from './SimpleEvents';
+// import SimpleEvents from './SimpleEvents';
+import EventAndState from './EventAndState';
 
 // function App() {
 //   return <h1>Sanity Check</h1>;
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <SimpleEvents />
+        <EventAndState />
       </div>
     );
   }
