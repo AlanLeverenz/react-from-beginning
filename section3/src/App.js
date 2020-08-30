5583f64057665f5d55ef725a2e4405d2f7fc3850
Alan Leverenz <awleverenz@aol.com>
2020-08-29 23:22:56 -0400

    updated app.js with componentDidMount

--- section3/src/App.js
+++ section3/src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-function App() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=7d4f2cda5bab002442683b32490d0c00`;
-  axios.get(url).then((res) => {
-    console.log(res);
-  });
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      temp: '',
+    };
+  }
+  render() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=7d4f2cda5bab002442683b32490d0c00`;
+    axios.get(url).then((res) => {
+      console.log(res);
+      this.setState({
+        temp: res.data.main.temp,
+      });
+    });
 
-  return (
-    <div className='App'>
-      <h1>Sanity Check</h1>
-    </div>
-  );
+    return (
+      <div className='App'>
+        <h1>{this.state.temp}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
