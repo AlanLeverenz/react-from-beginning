c2b1a4380dd708a700e0e7d6d533c58c8feee8e7
Alan Leverenz <awleverenz@aol.com>
2020-08-30 22:37:01 -0400

    inserted model code in componentDidMount

--- section3/src/App.js
+++ section3/src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
       temp: '',
     };
   }
-  render() {
+
+  // 7d4f2cda5bab002442683b32490d0c00`;
+  // fc675bd9b8d62b7af66e3d118b418449
+  componentDidMount() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=7d4f2cda5bab002442683b32490d0c00`;
     axios.get(url).then((res) => {
       console.log(res);
@@ -17,10 +20,37 @@ class App extends Component {
         temp: res.data.main.temp,
       });
     });
+    const elems = document.querySelectorAll('.modal');
+    const instances = window.M.Modal.init(elems);
+  }
 
+  render() {
     return (
       <div className='App'>
         <h1>{this.state.temp}</h1>
+        {/* <!-- Modal Trigger --> */}
+        <a
+          className='waves-effect waves-light btn modal-trigger'
+          href='#modal1'
+        >
+          Modal
+        </a>
+
+        {/* <!-- Modal Structure --> */}
+        <div id='modal1' className='modal'>
+          <div className='modal-content'>
+            <h4>Modal Header</h4>
+            <p>A bunch of text</p>
+          </div>
+          <div className='modal-footer'>
+            <a
+              href='#!'
+              className='modal-close waves-effect waves-green btn-flat'
+            >
+              Agree
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
