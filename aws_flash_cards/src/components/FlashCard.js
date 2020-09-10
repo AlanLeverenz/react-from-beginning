75aeca500a2d5b054c129e7bb3db00a97c53d59e
Alan Leverenz <awleverenz@aol.com>
2020-09-10 18:44:47 -0400

    mapped new quizTypes array in QuizBar component

---
+++ aws_flash_cards/src/components/FlashCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FlashCard extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Flashcard</h1>;
+  }
+}
+
+export default FlashCard;
