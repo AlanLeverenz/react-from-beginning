2d96fb5640dcd77809e733ffc00623096d7953c5
Alan Leverenz <awleverenz@aol.com>
2020-09-10 21:40:37 -0400

    inserted card components

--- aws_flash_cards/src/components/FlashCard.js
+++ aws_flash_cards/src/components/FlashCard.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import MultiCard from './MultiCard';
+import RegularCard from './RegularCard';
+import RandomWeighted from './RandomWeighted';
 
 class FlashCard extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      flipClass: '',
+    };
   }
 
+  // setState when flashcard is clicked
+  flip = (e) => {
+    let newFlip = this.state.flipClass === '' ? 'flip' : '';
+    this.setState({
+      flipClass: newFlip,
+    });
+  };
+
   render() {
-    return <h1>Flashcard</h1>;
+    return (
+      <div className='row align-items-center card-holder'>
+        <div
+          onClick={this.flip}
+          className={`col-sm-6 offset-sm-3 card mb-3 ${this.state.flipClass}`}
+        >
+          <RegularCard />
+        </div>
+      </div>
+    );
   }
 }
 
