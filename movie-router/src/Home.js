62b75db6fb1c58b191182bfc758a8712036d2acb
Alan Leverenz <awleverenz@aol.com>
2020-09-21 12:25:34 -0400

    installed axios, tested Home component

--- movie-router/src/Home.js
+++ movie-router/src/Home.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import config from './config';
 
-function Home(props) {
-  return <h1>Home page!</h1>;
+class Home extends Component {
+  componentDidMount() {
+    const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${config.api_key}`;
+    axios.get(nowPlayingUrl).then((response) => {
+      console.log(response.data);
+    });
+  }
+  render() {
+    return <h1>Home page!</h1>;
+  }
 }
 
 export default Home;
