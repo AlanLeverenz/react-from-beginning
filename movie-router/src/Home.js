23aab7c19d5479a84d5b22b8f2907c482a220e11
Alan Leverenz <awleverenz@aol.com>
2020-09-22 17:21:28 -0400

    setup Links to individual item id's

--- movie-router/src/Home.js
+++ movie-router/src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import config from './config';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   constructor() {
@@ -26,7 +27,9 @@ class Home extends Component {
     const movieGrid = this.state.movieList.map((movie, index) => {
       return (
         <div className='col s3' key={index}>
-          <img src={`${imageUrl}${movie.poster_path}`} alt='movie' />
+          <Link to={`/movie/${movie.id}`}>
+            <img src={`${imageUrl}${movie.poster_path}`} alt='movie' />
+          </Link>
         </div>
       );
     });
