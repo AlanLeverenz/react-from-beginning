1c681a697ca340c3734a94ab5468e729d9fddda1
Alan Leverenz <awleverenz@aol.com>
2020-09-25 22:08:59 -0400

    added component to App

---
+++ redux101/src/components/FrozenDept.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class FrozenDept extends Component {
+  render() {
+    return <h1>The frozen food department!</h1>;
+  }
+}
+
+export default FrozenDept;
