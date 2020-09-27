60445fa034af72125ab7a55dea21ad198671fef9
Alan Leverenz <awleverenz@aol.com>
2020-09-26 23:45:56 -0400

    exported connect function component

--- redux101/src/components/FrozenDept.js
+++ redux101/src/components/FrozenDept.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+// we want this component to know about redux.
+//  to do that, we some help.. or some glue
+// the glue is react-redux! we need the connect function
+import { connect } from 'react-redux';
+
 class FrozenDept extends Component {
   render() {
     return <h1>The frozen food department!</h1>;
   }
 }
 
-export default FrozenDept;
+// export default FrozenDept;
+export default connect()(FrozenDept);
