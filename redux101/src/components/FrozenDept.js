c7f9bd7438a51e4ffce570278a8e149af5885d3d
Alan Leverenz <awleverenz@aol.com>
2020-09-27 22:35:22 -0400

    connected Redux and React

--- redux101/src/components/FrozenDept.js
+++ redux101/src/components/FrozenDept.js
@@ -7,12 +7,25 @@ import { connect } from 'react-redux';
 
 class FrozenDept extends Component {
   render() {
+    console.log(this.props.frozenData);
     return <h1>The frozen food department!</h1>;
   }
 }
 
+// console.log(connect);
+// mapStateToProps takes 1 arg, "state" and that is the rootReducer/Store
+function mapStateToProps(state) {
+  // mapStateToProps returns an object, with:
+  // property is the local prop name to this component
+  // value will be the property in the root reducer... ie., a piece of the store
+  return {
+    frozenData: state.frozen,
+  };
+}
+
 // export default FrozenDept;
-export default connect()(FrozenDept);
+// connect takes 2 args, the first one is a function that is going to map a piece of redux state to this components props (matStateToProps)
+export default connect(mapStateToProps)(FrozenDept);
 
 // function x(n) {
 //   return (m) => {
--- redux101/src/reducers/frozen.js
+++ redux101/src/reducers/frozen.js
@@ -2,7 +2,9 @@
 // 1. Current State, usually provide a default state
 // 2. Info that came from any action
 
-export default (state = [], action) => {
+const seedData = ['TV Dinners', 'Frozen Veggies', 'Frozen Pizzas'];
+
+export default (state = seedData, action) => {
   return state;
 };
 
