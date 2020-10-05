440a68fac2ba9310f3f03bc9cbd867a72e45b9d8
Alan Leverenz <awleverenz@aol.com>
2020-10-05 14:38:38 -0400

    fixed meat reducer error

--- redux101/src/components/MeatDept.js
+++ redux101/src/components/MeatDept.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-
-// we want this component to know about redux.
-// to do that, we some help... or some glue
-// the glue is react-redux! we need the connect function
 import { connect } from 'react-redux';
 import updateMeat from '../actions/meatInvUpdate';
 import { bindActionCreators } from 'redux';
 
 class MeatDept extends Component {
   increment = (qChange, index) => {
-    // console.log(operation, index);
     this.props.updateMeat(qChange, index);
   };
 
@@ -21,6 +16,7 @@ class MeatDept extends Component {
             {item.food}: {item.quantity}
           </li>
           <input
+            className='add-button'
             type='button'
             onClick={() => {
               this.increment(1, i);
@@ -28,6 +24,7 @@ class MeatDept extends Component {
             value='+'
           />
           <input
+            className='subtract-button'
             type='button'
             onClick={() => {
               this.increment(-1, i);
@@ -39,32 +36,20 @@ class MeatDept extends Component {
     });
     return (
       <div>
-        <h1>The meat department!</h1>
+        <h1>The meat food department!</h1>
         <ul>{meatInventory}</ul>
       </div>
     );
   }
 }
 
-// console.log(connect);
-// mapStateToProps takes 1 arg, "state" and that is the rootReducer/Store
 function mapStateToProps(state) {
-  // mapStateToProps returns an object, with:
-  // property is the local prop name to this component
-  // value will be the property in the root reducer... ie., a piece of the store
   return {
     meatData: state.meat,
   };
 }
 
-// mapDispatchToProps is how we tie our component to the dispatch
-// takes 1 arg: dispatch
 function mapDispatchToProps(dispatch) {
-  // this function returns, bindActionCreators
-  // and we hand bindActionCreators an object:
-  // each property will be a local property
-  // each value will be a function that is dispatch when we run
-  // 2nd arg or bindActionCreators is the dispatch
   return bindActionCreators(
     {
       updateMeat: updateMeat,
@@ -72,16 +57,4 @@ function mapDispatchToProps(dispatch) {
     dispatch
   );
 }
-// export default FrozenDept;
-// connect takes 2 args, the first one is a function that is going to map a piece of redux state to this components props (mapStateToProps)
 export default connect(mapStateToProps, mapDispatchToProps)(MeatDept);
-
-// function x(n) {
-//   return (m) => {
-//     console.log(n+m);
-//     return(j)=>{
-//       console.log(n+m+j);
-//     }
-//   }
-// }
-// x(1)(2)(3);
--- redux101/src/reducers/meatReducer.js
+++ redux101/src/reducers/meatReducer.js
@@ -1,4 +1,4 @@
-// All reducers hvae 2 params:
+// All reducers have 2 params:
 // 1. Current State, usually provide a default state
 // 2. Info that came from any action
 
@@ -23,18 +23,17 @@ const seedData = [
 
 export default (state = seedData, action) => {
   console.log('Meat reducer is running! (Data)');
-  console.log(action);
   if (action.type === 'updateMeat') {
     console.log('I care about this action!!!');
     console.log(action);
     // we make a copy of state, becuase we never ever mutate state
-    if (action.type === 'updateMeat') {
-      const newState = [...state];
-      const payload = action.payload;
-      newState[payload.index].quantity += payload.qChange;
-      return newState;
-    } else {
-      return state;
-    }
+    // if (action.type === 'updateMeat') {
+    const newState = [...state];
+    const payload = action.payload;
+    newState[payload.index].quantity += payload.qChange;
+    return newState;
+  } else {
+    return state;
   }
 };
+// };
