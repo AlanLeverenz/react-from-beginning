14b92a1878515a141e07a8a4ece750b3d97e1585
Alan Leverenz <awleverenz@aol.com>
2020-10-09 21:48:06 -0400

    updated reference to openweathermap

--- redux-middleware-starter/src/actions/fetchWeather.js
+++ redux-middleware-starter/src/actions/fetchWeather.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const weatherApi = 'http://api.openweathermap.org/data/2.5/weather';
-const weatherAPIKey = '6f3f23c0f1a2fcb7edee25d08cb9cf62';
+// const weatherAPIKey = '6f3f23c0f1a2fcb7edee25d08cb9cf62';
+const weatherAPIKey = '7d4f2cda5bab002442683b32490d0c00';
 const scale = 'imperial'; //metric
 
 export default (city) => {
