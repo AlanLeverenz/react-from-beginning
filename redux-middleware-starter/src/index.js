be589b765e7b5d2be86b0740897db6109b3ae24a
Alan Leverenz <awleverenz@aol.com>
2020-10-09 22:24:28 -0400

    added redux-promise to handle aysnc await

--- redux-middleware-starter/src/index.js
+++ redux-middleware-starter/src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
+import reduxPromise from 'redux-promise';
 
-const theStore = createStore(rootReducer);
+const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
+
+// to break it down:
+// const middlewareApplied = applyMiddleware(reduxPromise);
+// const storeWithMiddleWare = middlewareApplied(createStore);
+// const finalStore = storeWithMiddleWare(rootReducer);
 
 ReactDOM.render(
   <Provider store={theStore}>
