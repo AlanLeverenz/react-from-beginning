ad6416c15d1a8dd89d0d2622142d4ef699626186
Alan Leverenz <awleverenz@aol.com>
2020-10-12 11:20:13 -0400

    setup NavBar and Home files

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import NavBar from './utility/NavBar/NavBar';
 
 class App extends Component {
   render() {
-    return <h1>App</h1>;
+    return (
+      <Router>
+        <Route path='/' component={NavBar} />
+        <Route exact path='/' component={Home} />
+      </Router>
+    );
   }
 }
 
