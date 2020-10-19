079935ab2cc7489cd672e9c193b57a26724b108f
Alan Leverenz <awleverenz@aol.com>
2020-10-19 14:42:17 -0400

    finished Activities component

--- airbnb-clone/src/utility/Activity/Activities.js
+++ airbnb-clone/src/utility/Activity/Activities.js
@@ -8,13 +8,14 @@ class Activities extends Component {
         console.log(this.props.activities);
         const activities = this.props.activities.map((activity,i)=>{
             return(
-                <div key={i} className="col s3">
+                <div key={i} className="col s2">
                     <Activity activity={activity} />
                 </div>
             )
         })
         return (
-            <div className="activity">
+            <div className="activities">
+            <h1 className="main-header-text">{this.props.header}</h1>
                 {activities}
             </div>
         )
--- airbnb-clone/src/utility/Activity/Activity.js
+++ airbnb-clone/src/utility/Activity/Activity.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Activity.css';
+import {Link} from 'react-router-dom';
 
 class Activity extends Component{
 
@@ -12,9 +13,21 @@ class Activity extends Component{
 // totalRatings: 131
 
     render(){
+        const { activityType, cost, id, image, rating, title, totalRatings } =  this.props.activity;
+        const starSize = {
+            fontSize: 12
+        }
         return(
             <div className="activity">
-                <h1>Activity</h1>
+            <Link to={`/activity/${id}`}>
+                <img src={image} alt="activity" />
+                <div className="activity-type">{activityType}</div>
+                <div className="title">{title}</div>
+                <div className="cost">${cost}/person</div>
+                <div className="rating">
+                <i className="material-icons" style={starSize}>star</i>{rating} ({totalRatings})
+                </div>
+            </Link>
             </div>
         )
     }
