0713a55f8ae8a43b791a204819439ea38033ad42
Alan Leverenz <awleverenz@aol.com>
2020-11-05 22:28:28 -0500

    started setup for Redux-Persist

--- airbnb-clone/src/index.js
+++ airbnb-clone/src/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+// Redux setup
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import reduxPromise from 'redux-promise';
 
+// Redux Persist setup
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
 
 ReactDOM.render(
   <Provider store={theStore}>
-    <App />
+    <PersistGate>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
