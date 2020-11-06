656e423b6add947137a147f9364e4fb06a00c9f9
Alan Leverenz <awleverenz@aol.com>
2020-11-06 12:45:45 -0500

    completed Redux-Persist (with Spinner)

--- airbnb-clone/src/index.js
+++ airbnb-clone/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Spinner from './utility/Spinner/Spinner';
 
 // Redux setup
 import { Provider } from 'react-redux';
@@ -12,11 +13,14 @@ import reduxPromise from 'redux-promise';
 // Redux Persist setup
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: autoMergeLevel2,
+  blacklist: ['siteModal',]
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
@@ -25,7 +29,7 @@ const persistor = persistStore(theStore);
 
 ReactDOM.render(
   <Provider store={theStore}>
-    <PersistGate persistor={persistor}>
+    <PersistGate loading={Spinner} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>,
