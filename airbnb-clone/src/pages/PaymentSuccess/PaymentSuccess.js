dfadd5a783374ab4f6c75a71b72881e4f9a7c512
Alan Leverenz <awleverenz@aol.com>
2020-11-20 22:54:24 -0500

    second update to PaymentSuccess.js

--- airbnb-clone/src/pages/PaymentSuccess/PaymentSuccess.js
+++ airbnb-clone/src/pages/PaymentSuccess/PaymentSuccess.js
@@ -17,12 +17,22 @@ class PaymentSuccess extends Component {
     }
 
     async componentDidMount(){
-
+        const stripeToken = this.props.match.params.stripeToken;
+        const token = this.props.auth.token;
+        const data = {stripeToken,token};
+        console.log(data);
     }
 
     render(){
         return(
-            <h1>PaymentSuccess</h1>
+            <div className="reservation-success row">
+                <h1 className="col m12 center">Start Packing!</h1>
+                <div className="resv-details col s8 offset-s2">
+                <div className="confirmed col m12 row">
+                    <FontAwesomeIcon icon="long-arrow-alt-right" size="1x" color="#ED0000" />Confirmed:     
+                </div>
+                </div>
+            </div>
 
         )
     }
