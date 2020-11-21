17fe3360cfb9c81dc8e005f5e5fd5159d28cbbce
Alan Leverenz <awleverenz@aol.com>
2020-11-20 22:44:35 -0500

    updated PaymentSuccess.js

--- airbnb-clone/src/pages/PaymentSuccess/PaymentSuccess.js
+++ airbnb-clone/src/pages/PaymentSuccess/PaymentSuccess.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react/react-FontAwesomeIcon';
+import moment from 'moment'
+import {Link} from 'react-router-dom'
+import Spinner from '../../utility/Spinner/Spinner'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import './PaymentSuccess.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import './PaymentSuccess.css'
 library.add(faLongArrowAltRight);
 
 class PaymentSuccess extends Component {
