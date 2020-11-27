05ad0411039e51214295fc70aba8d52efa60ed03
Alan Leverenz <awleverenz@aol.com>
2020-11-27 13:39:24 -0500

    updated Bookings.js with markup tables

--- airbnb-clone/src/pages/Account/Bookings.js
+++ airbnb-clone/src/pages/Account/Bookings.js
@@ -4,9 +4,48 @@ import swal from 'sweetalert';
 import axios from 'axios';
 
 function Bookings(props){
+
+    const bookings = props.bookings.map((booking, i)=>{
+        const dates = '';
+        return(
+            <tr key={i} className="booking-row">
+                <td>{booking.status}</td>
+                <td>
+                    <div className="booking-detail">{dates}</div>
+                    <div className="booking-detail">{booking.venueData.title}</div>
+                    <div className="booking-detail">{booking.venueData.location}</div>
+                </td>
+                <td>
+                    <div className="booking-detail">Confirmation #: {booking.conf}</div>
+                    <div className="booking-detail">{booking.numberOfGuests} Guests, {booking.totalNights} Nights</div>
+                    <div className="booking-detail">${booking.pricePerNight} per night</div>
+                    <div className="booking-detail">${booking.totalPrice} Total</div>
+                </td>
+                <td>
+                    <div className="booking-detail pointer">
+                        Print Reservation
+                    </div>
+                    <div className="booking-detail pointer">Cancel Confirmation</div>
+                </td>
+            </tr>
+        )
+    })
+
     console.log(props);
     return(
-        <h1>Bookings</h1>
+        <table className="booking">
+            <thead>
+                <tr>
+                    <th>Status</th>
+                    <th>Dates and location</th>
+                    <th>Details</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {bookings}
+            </tbody>
+        </table>
     )
 }
 
