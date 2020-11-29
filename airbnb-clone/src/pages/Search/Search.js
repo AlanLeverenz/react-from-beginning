484702747948e8cfd4be31f036817f232b1044a0
Alan Leverenz <awleverenz@aol.com>
2020-11-29 16:54:42 -0500

    added Search component

---
+++ airbnb-clone/src/pages/Search/Search.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Search.css';
+import '../Home/Home.css';
+import Spinner from '../../utility/Spinner/Spinner';
+import axios from 'axios';
+import Cities from '../../utility/City/Cities';
+import Activities from '../../utility/Activity/Activities';
+import Venues from '../../utility/Venue/Venues';
+
+class Search extends Component{
+
+    componentDidMount(){
+
+
+    }
+    
+    render(){
+        return (
+            <div className="container-fluid lower-fold">
+                <div className="row">
+                    <div className="col s12">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Search;
