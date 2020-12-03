219c32499f02622949faffc6cfd6e655cbda5f5e
Alan Leverenz <awleverenz@aol.com>
2020-12-03 12:28:04 -0500

    converted class to function in App.js

--- hooks101/src/App.js
+++ hooks101/src/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React , {useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  state = {counter: 0}
+// strict mode means using const or let
 
-  updateCounter = ()=>{
-    this.setState({counter: this.state.counter+1})
-  }
-  
-  render(){
+function App() {
+  const [counter,setCounter] = useState(0)
+
     return(
       <div>
-        <div>Counter: {this.state.counter}</div>
-        <div><button onClick={this.updateCounter}>Add 1!</button></div>
+        <div>Counter: {counter}</div>
+        <div><button onClick={()=>{setCounter(counter+1)}}>Add 1!</button></div>
       </div>
       )
   } 
-}
+
+
+// class App extends React.Component {
+//   state = {counter: 0}
+
+//   updateCounter = ()=>{
+//     this.setState({counter: this.state.counter+1})
+//   }
+  
+//   render(){
+//     return(
+//       <div>
+//         <div>Counter: {this.state.counter}</div>
+//         <div><button onClick={this.updateCounter}>Add 1!</button></div>
+//       </div>
+//       )
+//   } 
+// }
 
 export default App;
