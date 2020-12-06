da55c148df44f2ad9e0e25b51fd291631de59d26
Alan Leverenz <awleverenz@aol.com>
2020-12-06 14:24:44 -0500

    added state, form, and submit handler

--- hooks101/src/WeatherApp.js
+++ hooks101/src/WeatherApp.js
@@ -3,9 +3,31 @@ import './App.css';
 import CityWeather from './CityWeather';
 
 class WeatherApp extends Component {
+
+        state = {city: 'London'
+        }
+        // constructor() {
+        // super();
+        // this.state = {
+        //     city: 'London'
+        // }
+        // }
+
+    changeCity = (e)=>{
+        this.setState({city:e.target.value})
+    }
+
     render(){
         return(
-            <CityWeather />
+            <div className="container">
+                <CityWeather />
+                <div className="row justify-content-center">
+                    <form onSubmit={this.citysearch}>
+                        <input type="text" value={this.state.city} onChange={this.changeCity} />
+                        <input type="submit" className="btn btn-primary" value="Search!" />
+                    </form>
+                </div>
+            </div>
         )
     }
 }
