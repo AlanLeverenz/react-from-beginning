a9096df4951c01d7bbfd2c692a9e6457046ae28e
Alan Leverenz <awleverenz@aol.com>
2020-12-06 13:51:17 -0500

    added WeatherApp and CityWeather components

---
+++ hooks101/src/CityWeather.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class CityWeather extends Component {
+    render(){
+        return(
+            <h1>CityWeather</h1>
+        )
+    }
+}
+
+export default CityWeather;
---
+++ hooks101/src/WeatherApp.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class WeatherApp extends Component {
+    render(){
+        return(
+            <h1>WeatherApp</h1>
+        )
+    }
+}
+
+export default WeatherApp;
