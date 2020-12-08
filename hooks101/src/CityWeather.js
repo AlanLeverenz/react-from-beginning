348e818f1321759e5261dd1501395a2038cd2bc7
Alan Leverenz <awleverenz@aol.com>
2020-12-07 21:36:12 -0500

    updated CityWeather, iconurl problem

--- hooks101/src/CityWeather.js
+++ hooks101/src/CityWeather.js
@@ -4,7 +4,9 @@ import axios from 'axios';
 
 class CityWeather extends Component {
 
-    state = {}
+    state = {
+        cityData: {},
+    }
 
     async componentDidMount() {
         this.getWeather();
@@ -17,17 +19,28 @@ class CityWeather extends Component {
     }
 
     getWeather = async ()=>{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.props.cityName}&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.props.cityName}&units=imperial&appid=7d4f2cda5bab002442683b32490d0c00`;
+        // const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.props.cityName}&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d`;
         const resp = await axios.get(url);
+        this.setState({
+            cityData: resp.data,
+        })
         console.log(resp.data);
     }
 
     render(){
-        const iconUrl = `http://openweathermap.org/img/w/${this.state.icon}.png`
+        const iconUrl = `http://openweathermap.org/img/wn/${this.state.cityData.weather[0].icon}@2x.png`
         return(
-            <h1>{this.props.cityName}</h1>
+            <div className="container">
+                <div className="city-name">{this.state.cityData.name}</div>
+                <div className="temp">{this.state.cityData.main.temp} <img src={iconUrl} alt="icon"/></div>
+            </div>
         )
     }
 }
+
+// <div className="temp">{this.state.cityData.main.temp} <img src={iconUrl} alt="icon"/></div>
+
+// const iconUrl = `http://openweathermap.org/img/w/${this.state.cityData.weather[0].icon}.png`
 
 export default CityWeather;
