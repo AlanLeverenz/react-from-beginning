9334d7bdb41e8d36ae1fef560ddbad7fa32e05d6
Alan Leverenz <awleverenz@aol.com>
2020-12-08 01:58:41 -0500

    fixed CityWeather file

--- hooks101/src/CityWeather.js
+++ hooks101/src/CityWeather.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component, useState} from 'react';
 import axios from 'axios';
 
 class CityWeather extends Component {
+
 
     state = {
         cityData: {},
     }
 
-    async componentDidMount() {
+    async componentDidMount(){
         this.getWeather();
     }
 
@@ -18,18 +18,21 @@ class CityWeather extends Component {
         }
     }
 
-    getWeather = async ()=>{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.props.cityName}&units=imperial&appid=7d4f2cda5bab002442683b32490d0c00`;
-        // const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.props.cityName}&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d`;
+    getWeather = async()=>{
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.props.cityName}&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d`
         const resp = await axios.get(url);
         this.setState({
-            cityData: resp.data,
+            cityData:resp.data,
         })
         console.log(resp.data);
     }
 
     render(){
-        const iconUrl = `http://openweathermap.org/img/wn/${this.state.cityData.weather[0].icon}@2x.png`
+        if(!this.state.cityData.name){
+            return <h1>Loading...</h1>
+        }
+        const iconUrl = `http://openweathermap.org/img/w/${this.state.cityData.weather[0].icon}.png`
+
         return(
             <div className="container">
                 <div className="city-name">{this.state.cityData.name}</div>
@@ -39,8 +42,6 @@ class CityWeather extends Component {
     }
 }
 
-// <div className="temp">{this.state.cityData.main.temp} <img src={iconUrl} alt="icon"/></div>
-
-// const iconUrl = `http://openweathermap.org/img/w/${this.state.cityData.weather[0].icon}.png`
-
 export default CityWeather;
+
+// appid=7d4f2cda5bab002442683b32490d0c00
