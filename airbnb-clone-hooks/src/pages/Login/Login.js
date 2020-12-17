92355ad3cfda8cdbfcbecc95e833193d28a04675
Alan Leverenz <awleverenz@aol.com>
2020-12-16 21:50:15 -0500

    applied useDispatch to Login.js

--- airbnb-clone-hooks/src/pages/Login/Login.js
+++ airbnb-clone-hooks/src/pages/Login/Login.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 import './Login.css';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { useDispatch } from 'react-redux';
 import openModal from '../../actions/openModal';
+import regAction from '../../actions/regAction'
 import SignUp from './SignUp';
 import axios from 'axios';
-import regAction from '../../actions/regAction'
 import swal from 'sweetalert'
 
-class Login extends Component {
-    state = {
-        email: "",
-        password: "",
-    }
+function Login (props) {
+
+    const dispatch = useDispatch();
+
+    const [ email, changeEmail ] = useState[""];
+    const [ password, changePassword ] = useState[""];
 
     changeEmail = (e)=>this.setState({email:e.target.value})
     changePassword = (e)=>this.setState({password:e.target.value})
@@ -22,13 +22,6 @@ class Login extends Component {
         console.log(this.state.email);
         console.log(this.state.password);
 
-        // make an axios request to /users/login
-
-        //Handle:
-            // -- badPass
-            // -- noEmail
-            // -- loggedIn
-                // -- run regAction to /users/login
 
         const url = `${window.apiHost}/users/login`;
         const data = {
@@ -39,59 +32,57 @@ class Login extends Component {
         const token = resp.data.token;
         console.log(token);
 
-        // -- loggedIn
-        // -- badPass
         // -- noEmail
-
         if(resp.data.msg === "noEmail"){
             swal({
                 title: "Please provide an email",
                 icon: "error",
-              })            
+              })  
+        // -- badPass          
         }else if(resp.data.msg === "badPass"){
             swal({
                 title: "Invalid email/password",
                 text: "We don't have a match for that user name and password.",
                 icon: "error",
               })
+        // -- loggedIn
         }else if(resp.data.msg === "loggedIn"){
             swal({
                 title: "Success!",
                 icon: "success",
               });
             // we call our register action to update our auth reducer!!
+            // props.regAction(resp.data);
+            // window.setTimeout(()=>{
+                // dispatch({
+                //     type: "REGISTER_ACTION",
+                //     payload: resp.data,
+                // });
+            // },3000)
+            
             this.props.regAction(resp.data);
         }
     }
 
-    render(){
-        return(
-            <div className="login-form">
-                <form onSubmit={this.submitLogin}>
-                    <button className="facebook-login">Connect With Facebook</button>
-                    <button className="google-login">Connect With Google</button>
-                    <div className="login-or center">
-                        <span>or</span>
-                        <div className="or-divider"></div>
-                    </div>
-                    <input onChange={this.changeEmail} value={this.state.email} type="text" className="browser-default" placeholder="Email address" />
-                    <input onChange={this.changePassword} value={this.state.password} type="password" className="browser-default" placeholder="Password" />
-                    <button className="sign-up-button">Login</button>
+    return(
+        <div className="login-form">
+            <form onSubmit={this.submitLogin}>
+                <button className="facebook-login">Connect With Facebook</button>
+                <button className="google-login">Connect With Google</button>
+                <div className="login-or center">
+                    <span>or</span>
+                    <div className="or-divider"></div>
+                </div>
+                <input onChange={changeEmail} value={email} type="text" className="browser-default" placeholder="Email address" />
+                <input onChange={changePassword} value={password} type="password" className="browser-default" placeholder="Password" />
+                <button className="sign-up-button">Login</button>
 
-                    <div className="divider"></div>
-                    <div>Don't have an account? <span className="pointer" onClick={()=>{this.props.openModal('open', <SignUp />)}}>Sign Up</span>
-                    </div>
-                </form>
-            </div> 
-        )
-    }
+                <div className="divider"></div>
+                <div>Don't have an account? <span className="pointer" onClick={()=>dispatch(openModal('open', <SignUp />))}>Sign Up</span>
+                </div>
+            </form>
+        </div> 
+    )
 }
-
-function mapDispatchToProps(dispatcher){
-    return bindActionCreators({
-      openModal: openModal,
-      regAction: regAction
-    }, dispatcher)
-  }
   
-  export default connect(null, mapDispatchToProps)(Login);
+export default Login;
