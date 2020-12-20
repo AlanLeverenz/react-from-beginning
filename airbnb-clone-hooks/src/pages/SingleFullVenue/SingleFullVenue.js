30899a3eed91c1f91dce223346f04d7106b8cd6b
Alan Leverenz <awleverenz@aol.com>
2020-12-20 09:45:33 -0500

    refactored SingleFullVenue to hooks

--- airbnb-clone-hooks/src/pages/SingleFullVenue/SingleFullVenue.js
+++ airbnb-clone-hooks/src/pages/SingleFullVenue/SingleFullVenue.js
@@ -1,49 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SingleFullVenue.css';
 import axios from 'axios';
 import Point from './Point';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import openModal from '../../actions/openModal';
-import {bindActionCreators} from 'redux';
 import Login from '../Login/Login';
 import moment from 'moment';
 import swal from 'sweetalert';
 import loadScript from '../../utilityFunctions/loadScript';
 
-class SingleFullVenue extends Component {
+function SingleFullVenue(props) {
 
-    state = {
-        singleVenue: {},
-        points: [],
-        checkIn: [],
-        checkOut: [],
-        numberOfGuests: 1
-    }
+    const dispatch = useDispatch();
+    const token = useSelector(state => state.auth.token);
 
-async componentDidMount() {
-    const vId = this.props.match.params.vid;
-    const url = `${window.apiHost}/venue/${vId}`;
-    const axiosResponse = await axios.get(url);
-    const singleVenue = axiosResponse.data;
+    const [ singleVenue, setSingleVenue ] = useState({})
+    const [ points, setPoints ] = useState([])
+    const [ checkIn, setCheckIn ] = useState("");
+    const [ checkOut, setCheckOut ] = useState("");
+    const [ numberOfGuests, setNumGuests ] = useState("");
 
-    const pointsUrl = `${window.apiHost}/points/get`;
-    const pointsAxiosResponse = await axios.get(pointsUrl);
-
-    const points = singleVenue.points.split(',').map((point,i)=>{
-        return(<Point key={i} pointDesc={pointsAxiosResponse.data} point={point} />)
+    useEffect(() => {
+        const vId = props.match.params.vid;
+        const url = `${window.apiHost}/venue/${vId}`;
+        // need sub-function to set up async await
+        const fetchData = async()=>{
+            const axiosResponse = await axios.get(url);
+            const singleVenue = axiosResponse.data;
+        
+            const pointsUrl = `${window.apiHost}/points/get`;
+            const pointsAxiosResponse = await axios.get(pointsUrl);
+        
+            const points = singleVenue.points.split(',').map((point,i)=>{
+                return(<Point key={i} pointDesc={pointsAxiosResponse.data} point={point} />)
+            })
+            setSingleVenue(singleVenue)
+            setPoints(points)
+        };
+        fetchData()
     })
-    this.setState({singleVenue,points});
-}
 
-    changeNumberOfGuests = (e)=>{this.setState({numberOfGuests: e.target.value})}
-    changeCheckIn = (e)=>{this.setState({checkIn: e.target.value})}
-    changeCheckOut = (e)=>{this.setState({checkOut: e.target.value})}
-
-    reserveNow = async(e) => {
-        const startDayMoment = moment(this.state.checkIn);
-        const endDayMoment = moment(this.state.checkOut);
+    const reserveNow = async(e) => {
+        const startDayMoment = moment(checkIn);
+        const endDayMoment = moment(checkOut);
         const diffDays = endDayMoment.diff(startDayMoment, "days");
-        console.log(diffDays);
+        // console.log(diffDays);
         if(diffDays < 1){
             //check in date must be before checkout date
             swal({
@@ -58,35 +59,23 @@ async componentDidMount() {
             })
         } else {
             // diff days is a valid number. get the price.
-            const pricePerNight = this.state.singleVenue.pricePerNight;
+            const pricePerNight = singleVenue.pricePerNight;
             const totalPrice = pricePerNight * diffDays;
             const scriptUrl = 'https://js.stripe.com/v3';
             const stripePublicKey = 'pk_test_5198HtPL5CfCPYJ3X8TTrO06ChWxotTw6Sm2el4WkYdrfN5Rh7vEuVguXyPrTezvm3ntblRX8TpjAHeMQfHkEpTA600waD2fMrT';
-            // Moving the below code to it's own module
-            // await new Promise((resolve, reject)=>{
-            //     const script = document.createElement('script');
-            //     script.type = 'text/javascript';
-            //     script.src = scriptUrl;
-            //     script.onload = ()=>{
-            //         console.log("The script has loaded!")
-            //         resolve();
-            //     }
-            //     document.getElementsByTagName('head')[0].appendChild(script);
-            //     console.log('The script has been added to the head!');
-            // })
             await loadScript(scriptUrl); // we don't need a variable, we just need to wait
             console.log("Let's run some Stripe");
             const stripe = window.Stripe(stripePublicKey);
             const stripeSessionUrl = `${window.apiHost}/payment/create-session`;
             const data = {
-                venueData: this.state.singleVenue,
+                venueData: singleVenue,
                 totalPrice,
                 diffDays,
                 pricePerNight,
-                checkIn: this.state.checkIn,
-                checkOut: this.state.checkOut,
-                token: this.props.auth.token,
-                numberOfGuests: this.state.numberOfGuests,
+                checkIn: checkIn,
+                checkOut: checkOut,
+                token: token,
+                numberOfGuests: numberOfGuests,
                 currency: 'USD',
             }
             
@@ -101,76 +90,63 @@ async componentDidMount() {
         }
     }
 
-    render(){
-        console.log(this.props.auth);
+    const sv = singleVenue;
 
-        console.log(this.state.singleVenue);
-        const sv = this.state.singleVenue;
-        return(
-            <div className="row single-venue">
-                <div className="col s12 center">
-                    <img src={sv.imageUrl} alt='venue' />
-                </div>
-                <div className="col s8 location-details offset-s2">
-                    <div className="col s8 left-details">
-                        <div className="location">{sv.location}</div>
-                        <div className="title">{sv.title}</div>
-                        <div className="guests">{sv.guests}</div>
-
-                        <div className="divider"></div>
-
-                        {this.state.points}
-
-                        <div className="details">{sv.details}</div>
-                        <div className="amenities">{sv.amenities}</div>
-                    </div>
-
-                    <div className="col s4 right-details">
-                        <div className="price-per-day">${sv.pricePerNight} <span>per day</span></div>
-                        <div className="rating">{sv.rating}</div>
-                        <div className="col s6">Check-In
-                        <input type="date" onChange={this.changeCheckIn} value={this.state.checkIn} />
-                        </div>
-                        <div className="col s6">Check-Out
-                        <input type="date" onChange={this.changeCheckOut} value={this.state.checkOut} />
-                        </div>
-                        <div className="col s12">
-                        <select className="browser-default" onChange={this.changeNumberOfGuests} value={this.state.numberOfGuests}>
-                                <option value="1">1 Guest</option>
-                                <option value="2">2 Guests</option>
-                                <option value="3">3 Guests</option>
-                                <option value="4">4 Guests</option>
-                                <option value="5">5 Guests</option>
-                                <option value="6">6 Guests</option>
-                                <option value="7">7 Guests</option>
-                                <option value="8">8 Guests</option>
-                            </select>
-                        </div>
-                        <div className="col s12 center">
-                        {this.props.auth.token ?
-                            <button onClick={this.reserveNow} className="btn red accent-2">Reserve</button>
-                        : <div>You must <span className="text-link" onClick={()=>{this.props.openModal('open',<Login />)}}>Log in</span> to reserve</div>
-                        }
-                        </div>
-                    </div>
-
-                </div>
+    return(
+        <div className="row single-venue">
+            <div className="col s12 center">
+                <img src={sv.imageUrl} alt='venue' />
             </div>
-        )
-    }
+            <div className="col s8 location-details offset-s2">
+                <div className="col s8 left-details">
+                    <div className="location">{sv.location}</div>
+                    <div className="title">{sv.title}</div>
+                    <div className="guests">{sv.guests}</div>
+
+                    <div className="divider"></div>
+
+                    {points}
+
+                    <div className="details">{sv.details}</div>
+                    <div className="amenities">{sv.amenities}</div>
+                </div>
+
+                <div className="col s4 right-details">
+                    <div className="price-per-day">${sv.pricePerNight} <span>per day</span></div>
+                    <div className="rating">{sv.rating}</div>
+
+                    <div className="col s6">Check-In
+                        <input type="date" onChange={(e)=>setCheckIn(e.target.value)} value={checkIn} />
+                    </div>
+
+                    <div className="col s6">Check-Out
+                        <input type="date" onChange={(e)=>setCheckOut(e.target.value)} value={checkOut} />
+                    </div>
+
+                    <div className="col s12">
+                        <select className="browser-default" onChange={(e)=>setNumGuests(e.target.value)} value={numberOfGuests}>
+                            <option value="1">1 Guest</option>
+                            <option value="2">2 Guests</option>
+                            <option value="3">3 Guests</option>
+                            <option value="4">4 Guests</option>
+                            <option value="5">5 Guests</option>
+                            <option value="6">6 Guests</option>
+                            <option value="7">7 Guests</option>
+                            <option value="8">8 Guests</option>
+                        </select>
+                    </div>
+
+                    <div className="col s12 center">
+                    {token ?
+                        <button onClick={reserveNow} className="btn red accent-2">Reserve</button>
+                    : <div>You must <span className="text-link" onClick={()=>dispatch(openModal('open',<Login />))}>Log in</span> to reserve</div>
+                    }
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
-function mapStateToProps(state){
-    return {
-        auth: state.auth,
-    }
-}
-
-function mapDispatchToProps(dispatch){
-    return bindActionCreators({
-        openModal
-    },dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SingleFullVenue);
-
+export default SingleFullVenue;
