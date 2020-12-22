41f82af27feb26d4bb47dc60e5c840aa0e851382
Alan Leverenz <awleverenz@aol.com>
2020-12-21 21:42:15 -0500

    refactored utilit/Venue.js to function

--- airbnb-clone-hooks/src/utility/Venue/Venue.js
+++ airbnb-clone-hooks/src/utility/Venue/Venue.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Venue extends Component{
+function Venue(props){
 
-    render(){
-        console.log(this.props.venue);
-        const { id, title, location, pricePerNight, imageUrl, rating } = this.props.venue;
-        const starSize = {
-            fontSize: 12
-        }
-        return(
-            <div className="venue col s12">
-                <Link to={`/venue/${id}`}>
-                    <div className="image">
-                        <img src={imageUrl} alt="venue"/>
-                    </div>
-                    <div className="location-stars">
-                        <span className="location">{location}</span>
-                        <span className="rating right"><i className="material-icons" style={starSize}>star</i>{rating}</span>
-                    </div>
-                    <div className="title">${title}</div>
-                    <div className="price-per-night">${pricePerNight}/night</div>
-                </Link>
-            </div>
-        )
+    console.log(props.venue);
+    const { id, title, location, pricePerNight, imageUrl, rating } = props.venue;
+    const starSize = {
+        fontSize: 12
     }
+    return(
+        <div className="venue col s12">
+            <Link to={`/venue/${id}`}>
+                <div className="image">
+                    <img src={imageUrl} alt="venue"/>
+                </div>
+                <div className="location-stars">
+                    <span className="location">{location}</span>
+                    <span className="rating right"><i className="material-icons" style={starSize}>star</i>{rating}</span>
+                </div>
+                <div className="title">${title}</div>
+                <div className="price-per-night">${pricePerNight}/night</div>
+            </Link>
+        </div>
+    )
 }
 
 export default Venue;
