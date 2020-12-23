60390fbe76afa89eba6b8eade71fc45e464739c4
Alan Leverenz <awleverenz@aol.com>
2020-12-23 09:55:48 -0500

    refactored Modal.js to hooks function

--- airbnb-clone-hooks/src/utility/Modal/Modal.js
+++ airbnb-clone-hooks/src/utility/Modal/Modal.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import {bindActionCreators} from 'redux';
 import './Modal.css';
 import openModal from '../../actions/openModal';
 
-class Modal extends Component{
+function Modal(props){
 
-    state = {
+    const dispatch = useDispatch();
+    // const content = useSelector(state => state.siteModal.content);
+    const siteModal = useSelector(state => state.siteModal);
 
-    }
-
-    closeModal = ()=> {
-        this.props.openModal('closed','');
-    }
+    // const closeModal = ()=> {
+    //     props.openModal('closed','');
+    // }
     // in place of line 32:
     // <span onClick={this.closeModal} className="close">&times;</span>
 
-    render(){
-
         let modalInlineStyle;
-        if(this.props.siteModal.openClose === "open"){
+        if(siteModal.openClose === "open"){
+        // if(props.siteModal.openClose === "open"){
             modalInlineStyle = {display: 'block'}
-        }else if(this.props.siteModal.openClose === "closed"){
+        }else if(siteModal.openClose === "closed"){
+        // }else if(props.siteModal.openClose === "closed"){
             modalInlineStyle = {display: 'none'};
         }
 
@@ -29,28 +29,30 @@ class Modal extends Component{
             <div className="site-modal" style={modalInlineStyle}>
                 <div className="modal-content">
                     <div className="col right">
-                        <span onClick={()=>{this.props.openModal('closed','')}} className="close">&times;</span>
+                        <span onClick={()=>dispatch(openModal('closed',''))} className="close">&times;</span>
 
                     </div>
                     <div className="">
-                        {this.props.siteModal.content}
+                        {siteModal.content}
                     </div>
                 </div>
             </div>
         )
     }
-}
 
-function mapStateToProps(state){
-    return{
-        siteModal: state.siteModal
-    }
-}
 
-function mapDispatchToProps(dispatcher){
-    return bindActionCreators({
-        openModal: openModal
-    },dispatcher)
-}
+// function mapStateToProps(state){
+//     return{
+//         siteModal: state.siteModal
+//     }
+// }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Modal)
+// function mapDispatchToProps(dispatcher){
+//     return bindActionCreators({
+//         openModal: openModal
+//     },dispatcher)
+// }
+
+// export default connect(mapStateToProps,mapDispatchToProps)(Modal)
+
+export default Modal;
