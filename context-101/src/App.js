9715ec2da9a6777d371f2fd8b96114d547bf76a7
Alan Leverenz <awleverenz@aol.com>
2020-12-24 08:02:07 -0500

    updated App with Context.Provider

--- context-101/src/App.js
+++ context-101/src/App.js
@@ -1,19 +1,23 @@
-import React, { useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import ThemeContext from './contexts/themeContext';
 import MainContainer from './MainContainer'
 
 function App() {
 
   // MainContainer can get the theme directly from themeContext
-  const theme = useContext(ThemeContext);
-  console.log(theme);
+  // const theme = useContext(ThemeContext);
+  // console.log(theme);
+
+  const [ theme, setTheme ] = useState("Blue");
 
   return (
-    <div>
+    <ThemeContext.Provider value={theme}>
       <h1>App component</h1>
+      <button onClick={(e)=>setTheme(theme === "Blue" ? "Red" : "Blue")}>
+        {theme === "Blue" ? "Switch to Red" : "Switch to Blue"}
+      </button>
       <MainContainer />
-    </div>
-
+      </ThemeContext.Provider>
     );
 }
 
